e3dfd8cc13c8a87b0445b340e35e11212822c3e6
johnliu1122 <johnliu1122@163.com>
2020-08-30 11:49:23 +0800

    init

---
+++ src/main/java/com/liuapi/service/identity/IdentityExhaustedException.java
@@ -0,0 +1,13 @@
+package com.liuapi.service.identity;
+
+/**
+ * 1、取号段
+ * 2、双buffer设计
+ * @auther 柳俊阳
+ * @github https://github.com/johnliu1122/
+ * @csdn https://blog.csdn.net/qq_35695616
+ * @email johnliu1122@163.com
+ * @date 2020/8/29
+ */
+public class IdentityExhaustedException extends RuntimeException{
+}
---
+++ src/main/java/com/liuapi/service/identity/Segment.java
@@ -0,0 +1,25 @@
+package com.liuapi.service.identity;
+
+/**
+ * @auther 柳俊阳
+ * @github https://github.com/johnliu1122/
+ * @csdn https://blog.csdn.net/qq_35695616
+ * @email johnliu1122@163.com
+ * @date 2020/8/29
+ */
+public class Segment {
+    private long min;
+    private long max;
+    private long next;
+    public Segment(long min,long max){
+        this.min = min;
+        this.max = max;
+        this.next = min;
+    }
+    public synchronized long retrieve() throws IdentityExhaustedException{
+        if(next<=max){
+            return next++;
+        }
+        throw new IdentityExhaustedException();
+    }
+}
